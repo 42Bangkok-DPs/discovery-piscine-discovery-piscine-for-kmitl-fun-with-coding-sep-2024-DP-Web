853220a68a6af6b5f12c953b2a7ff2365ee4909c
rsurin-221 <64070221@kmitl.ac.th>
2024-09-13 14:19:53 +0700

    Update todo.js

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -3,16 +3,40 @@ document.addEventListener('DOMContentLoaded', function() {
     const newTodoInput = document.getElementById('new-todo');
     const addButton = document.getElementById('new-btn');
 
+    function saveTodos() {
+        const todos = [];
+        todoList.querySelectorAll('li').forEach(li => {
+            todos.push(li.firstChild.textContent.trim());
+        });
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }
+
+    function loadTodos() {
+        const savedTodos = localStorage.getItem('todos');
+        if (savedTodos) {
+            const todos = JSON.parse(savedTodos);
+            todos.forEach(todoText => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    ${todoText}
+                    <button onclick="this.parentElement.remove(); saveTodos();">Delete</button>
+                `;
+                todoList.insertBefore(li, todoList.firstChild);
+            });
+        }
+    }
+
     function addTodo() {
         const todoText = newTodoInput.value.trim();
         if (todoText) {
             const li = document.createElement('li');
             li.innerHTML = `
                 ${todoText}
-                <button onclick="this.parentElement.remove()">Delete</button>
+                <button onclick="this.parentElement.remove(); saveTodos();">Delete</button>
             `;
             todoList.insertBefore(li, todoList.firstChild);
             newTodoInput.value = '';
+            saveTodos(); // Save after adding a new todo
         }
     }
 
@@ -23,4 +47,6 @@ document.addEventListener('DOMContentLoaded', function() {
             addTodo();
         }
     });
+
+    loadTodos();
 });
