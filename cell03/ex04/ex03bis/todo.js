5cdb40e63e5bfab67437548b3088a12c694f1a5a
rsurin-221 <64070221@kmitl.ac.th>
2024-09-13 14:27:11 +0700

    Update todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
-    const $todoList = $('#ft_list');
-    const $newTodoInput = $('#new-todo');
-    const $addButton = $('#new-btn');
+        const $todoList = $('#ft_list');
+        const $newTodoInput = $('#new-todo');
+        const $addButton = $('#new-btn');
 
-    function addTodo() {
-        const todoText = $newTodoInput.val().trim();
-        if (todoText) {
-            const $li = $('<li>').html(`
-                ${todoText}
-                <button>Delete</button>
-            `);
-            $li.find('button').on('click', function() {
-                $(this).parent().remove();
-            });
-            $todoList.prepend($li);
-            $newTodoInput.val('');
+        function addTodo() {
+            const todoText = $newTodoInput.val().trim();
+            if (todoText) {
+                const $li = $('<li>').html(`
+                    ${todoText}
+                    <button>Delete</button>
+                `);
+                $li.find('button').on('click', function() {
+                    $(this).parent().remove();
+                });
+                $todoList.prepend($li);
+                $newTodoInput.val('');
+            }
         }
-    }
 
-    $addButton.on('click', addTodo);
+        $addButton.on('click', addTodo);
 
-    $newTodoInput.on('keypress', function(e) {
-        if (e.which === 13) {
-            addTodo();
-        }
+        $newTodoInput.on('keypress', function(e) {
+            if (e.which === 13) {
+                addTodo();
+            }
+        });
     });
-});
