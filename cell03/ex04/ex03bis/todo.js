68764c2f253bdd181f5ad0f03742a2b82e334260
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2024-09-14 01:19:27 +0000

    finish

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,20 +1,43 @@
 $(document).ready(function() {
-    // Load saved todos from localStorage and render them
+    // Set a cookie
+    const setCookie = (name, value, days) => {
+        let expires = "";
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    };
+
+    // Get a cookie by name
+    const getCookie = (name) => {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for(let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    };
+
+    // Load saved todos from cookies and render them
     const loadTodos = () => {
-        const todos = JSON.parse(localStorage.getItem('todos')) || [];
+        const todos = JSON.parse(getCookie('todos')) || [];
         $('#ft_list').empty(); // Clear the list first
         todos.forEach(todo => {
             $('#ft_list').append(`<div class="todo">${todo}</div>`); // Append to the bottom
         });
     };
 
-    // Save todos to localStorage
+    // Save todos to cookies
     const saveTodos = () => {
         const todos = [];
         $('.todo').each(function() {
             todos.push($(this).text());
         });
-        localStorage.setItem('todos', JSON.stringify(todos));
+        setCookie('todos', JSON.stringify(todos), 7); // Save cookies for 7 days
     };
 
     // Add new todo
